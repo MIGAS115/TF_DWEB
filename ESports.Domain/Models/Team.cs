74d9c6c3cc7f959e11e45bc273af10ce0ada4112
MIGAS115 <migasafonso115@gmail.com>
2026-05-10 19:12:03 +0100

    Adição de classes, migração e criação de data base

---
+++ ESports.Domain/Models/Team.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace ESports.Domain.Models;
+
+/// <summary>
+/// Entidade que representa uma equipa de E-sports.
+/// </summary>
+public class Team
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O {0} é obrigatório")]
+    [StringLength(50, ErrorMessage = "O {0} não pode exceder {1} caracteres.")]
+    [Display(Name = "Nome da Equipa")]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Indica se o registo foi inserido/editado manualmente (Redundância/Defesa).
+    /// </summary>
+    public bool IsManualOverride { get; set; }
+
+    /// <summary>
+    /// ID proveniente da API externa.
+    /// </summary>
+    public string? ExternalSourceId { get; set; }
+
+    public ICollection<Match> HomeMatches { get; set; } = [];
+    public ICollection<Match> AwayMatches { get; set; } = [];
+    public ICollection<UserFavoriteTeam> FavoritedByUsers { get; set; } = [];
+}
---
+++ ESports.Domain/Models/UserFavoriteTeam.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESports.Domain.Models;
+
+/// <summary>
+/// Tabela intermédia N:M entre Utilizadores e Equipas.
+/// </summary>
+[PrimaryKey(nameof(MyUserFK), nameof(TeamFK))]
+public class UserFavoriteTeam
+{
+    [Display(Name = "Data de Adição aos Favoritos")]
+    public DateTime AddedAt { get; set; } = DateTime.Now;
+
+    [ForeignKey(nameof(User))]
+    public string MyUserFK { get; set; } = null!;
+    public MyUser User { get; set; } = null!;
+
+    [ForeignKey(nameof(Team))]
+    public int TeamFK { get; set; }
+    public Team Team { get; set; } = null!;
+}
